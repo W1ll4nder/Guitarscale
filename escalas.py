6a9eb0f1ca753e8cd78fca5a1d02e35d78863ab9
W1ll4nder <willanderguitar@hotmail.com>
2017-10-08 22:46:53 -0300

    commit2

--- escalas.py
+++ escalas.py
@@ -14,16 +14,76 @@ if tom in notas:
     if tom == notas[1]:
         notas1 = notas[1:]
         notas2 = notas[0]
-        notas2+= str(notas2)
         esc = notas1 + notas2
         notas = esc
         maior()
-
     if tom == notas[2]:
         notas1 = notas[2:]
         notas2 = notas[0:2]
         esc = notas1 + notas2
         notas = esc
         maior()
+    if tom == notas[3]:
+        notas1 = notas[3:]
+        notas2 = notas[0:3]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[4]:
+        notas1 = notas[4:]
+        notas2 = notas[0:4]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[5]:
+        notas1 = notas[5:]
+        notas2 = notas[0:5]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[6]:
+        notas1 = notas[6:]
+        notas2 = notas[0:6]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[7]:
+        notas1 = notas[7:]
+        notas2 = notas[0:7]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[8]:
+        notas1 = notas[8:]
+        notas2 = notas[0:8]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[9]:
+        notas1 = notas[9:]
+        notas2 = notas[0:9]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[10]:
+        notas1 = notas[10:]
+        notas2 = notas[0:10]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+    if tom == notas[11]:
+        notas1 = notas[11:]
+        notas2 = notas[0:11]
+        esc = notas1 + notas2
+        notas = esc
+        maior()
+else:
+    print('digite uma nota válida')
+
+
+
+
+
+
 
 
