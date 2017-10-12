48a22cbc28328da8d452484ca91de149f025c113
W1ll4nder <willanderguitar@hotmail.com>
2017-10-12 17:00:19 -0300

    imprimindo a eescala maior e a menor

--- escalas.py
+++ escalas.py
@@ -1,13 +1,16 @@
 notas_original = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 notas = notas_original
 def maior():
-    esc1 = (notas[0:6:2])
-    esc2 = (notas[5:12:2])
-    print(esc1 + esc2)
+    if escala == 'maior':
+        esc1 =notas[0]+notas[2]+notas[4]+notas[5]+notas[7]+notas[9]+notas[11]
+        print(esc1)
+    if escala == 'menor':
+        esc1 = notas[0]+notas[2]+notas[3]+notas[5]+notas[7]+notas[8]+notas[10]
+        print(esc1)
 cont = 'sim'
 while cont == 'sim':
     tom = input('digite o tom da escala que vc deseja: ').upper()
-
+    escala = input('qual escala vc deseja saber? ').lower()
     if tom in notas:
         posição = notas_original.index(tom)
         notas = notas_original[posição:] + notas_original[:posição]
